6ead80760ea5b7150319d55f7aa625cbb9ebd9c9
stevenspiegel <140023101+stevenspiegel@users.noreply.github.com>
2026-05-28 20:53:11 -0500

    Add elephant sprites

--- src/sprites/images.ts
+++ src/sprites/images.ts
@@ -131,6 +131,12 @@ export const IMAGE_SPRITES: Record<string, StageImages> = {
     teen: require('../../assets/sprites/tiger-teen.png'),
     adult: require('../../assets/sprites/tiger-adult.png'),
   },
+  '🐘': {
+    baby: require('../../assets/sprites/elephant-baby.png'),
+    child: require('../../assets/sprites/elephant-child.png'),
+    teen: require('../../assets/sprites/elephant-teen.png'),
+    adult: require('../../assets/sprites/elephant-adult.png'),
+  },
 };
 
 export const imageSpriteFor = (
